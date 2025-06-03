8b1223ff247857d30a47bda05840cd7c739a42bd
einsteinke <einstenmarto30@gmail.com>
2025-06-03 20:22:21 +0300

    fixed css issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // <--- This is the correct React plugin
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <--- This is the correct React plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], // <--- ONLY react() plugin here, unless you've added others for specific, *known* reasons
+  plugins: [react(),tailwindcss(),], // <--- ONLY react() plugin here, unless you've added others for specific, *known* reasons
   build: {
     outDir: 'dist',
   },
